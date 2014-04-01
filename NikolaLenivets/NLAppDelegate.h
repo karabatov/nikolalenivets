7e4c44cb67cf90ecb2dd0ed0fa1824caf69ab25b
Semyon Novikov <novikov.s@prognoz.ru>
2014-04-01 11:21:42 +0600

    Initial Commit

---
+++ NikolaLenivets/NLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NLAppDelegate.h
+//  NikolaLenivets
+//
+//  Created by Semyon Novikov on 01.04.14.
+//  Copyright (c) 2014 Semyon Novikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NikolaLenivets/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NikolaLenivets
+//
+//  Created by Semyon Novikov on 01.04.14.
+//  Copyright (c) 2014 Semyon Novikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NLAppDelegate class]));
+    }
+}
