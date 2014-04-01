19f7d7ec1e58324f4c70f2b061809ebcfafdb79e
Semyon Novikov <novikov.s@prognoz.ru>
2014-04-01 13:31:36 +0600

    Add backend spec

--- NikolaLenivetsTests/NLBackendSpec.m
+++ NikolaLenivetsTests/NLBackendSpec.m
@@ -8,6 +8,65 @@
 
 #import "Specta.h"
 #import "Expecta.h"
+#import "AFNetworking.h"
+
+static AFHTTPRequestOperationManager *manager;
 
 SpecBegin(BackendConnectionTest)
+
+describe(@"Backend connection", ^{
+    
+    beforeAll(^{
+        manager = [AFHTTPRequestOperationManager manager];
+    });
+    
+    it(@"should have backend url", ^{
+        expect(BACKEND_URL).toNot.beNil();
+    });
+    
+    
+    it(@"should be able to talk to backend", ^AsyncBlock {
+        [manager HEAD:BACKEND_URL
+           parameters:nil
+              success:^(AFHTTPRequestOperation *op) {
+                  expect(op).toNot.beNil();
+                  done();
+              }
+              failure:^(AFHTTPRequestOperation *op, NSError *error) {
+                  XCTFail(@"Can't talk to backend :(");
+                  done();
+              }];
+    });
+    
+    it(@"should receive not empty JSON response from backend", ^AsyncBlock {
+        [manager GET:BACKEND_URL
+          parameters:nil
+             success:^(AFHTTPRequestOperation *op, id response) {
+                 expect(response).toNot.beNil();
+                 expect(response).to.beKindOf([NSDictionary class]);
+                 done();
+             }
+             failure:^(AFHTTPRequestOperation *op, NSError *error) {
+                 XCTFail(@"Received error from backend");
+                 done();
+             }];
+    });
+    
+    it(@"should receive index object from backend when timestamp is specified", ^AsyncBlock {
+        [manager GET:[NSString stringWithFormat:@"%@/1049181727", BACKEND_URL]
+          parameters:nil
+             success:^(AFHTTPRequestOperation *op, NSDictionary *response) {
+                 expect(response[@"index"]).toNot.beNil();
+                 expect(response[@"index"]).to.beKindOf([NSDictionary class]);
+                 expect(response[@"index"][@"events"]).to.beKindOf([NSArray class]);
+                 expect([response[@"index"][@"events"] count]).to.beGreaterThan(0);
+                 done();
+             }
+             failure:^(AFHTTPRequestOperation *op, NSError *error) {
+                 XCTFail(@"Received error from backend");
+                 done();
+             }];
+    });
+});
+
 SpecEnd
