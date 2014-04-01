ddb0a169dfc4cc4ff354a46bfa0d50b14355d75f
Semyon Novikov <novikov.s@prognoz.ru>
2014-04-01 12:40:04 +0600

    Add Specta. Expecta. Now they can see proper headers. Add backend spec stub

---
+++ NikolaLenivetsTests/NLBackendSpec.m
@@ -0,0 +1,13 @@
+//
+//  NLBackendSpec.m
+//  NikolaLenivets
+//
+//  Created by Semyon Novikov on 01.04.14.
+//  Copyright (c) 2014 Semyon Novikov. All rights reserved.
+//
+
+#import "Specta.h"
+#import "Expecta.h"
+
+SpecBegin(BackendConnectionTest)
+SpecEnd
--- Podfile
+++ Podfile
@@ -1,3 +1,5 @@
 platform :ios, '6.0'
 
 pod 'AFNetworking', '~> 2.2.1'
+pod 'Specta', '~> 0.2.1'
+pod 'Expecta', '~> 0.3.0'
