e06a50f3c00260d0848f9a5a41024d3b4fdd5057
Semyon Novikov <novikov.s@prognoz.ru>
2014-04-04 17:13:19 +0600

    Add serialization specs

--- NikolaLenivetsTests/NLStorageSpec.m
+++ NikolaLenivetsTests/NLStorageSpec.m
@@ -54,6 +54,59 @@ describe(@"Storage", ^{
         expect(_storage.places.count).to.beGreaterThan(0);
         expect(_storage.screens.count).to.beGreaterThan(0);
     });
+    
+    it (@"should properly deserialize itself and all properties", ^{
+        expect([_storage respondsToSelector:@selector(populateWithDictionary:)]).to.beTruthy();
+        [_storage populateWithDictionary:_testDataDict];
+        
+        NLNewsEntry *newsEntry = _storage.news[0];
+        expect(newsEntry).to.beKindOf([NLNewsEntry class]);
+        expect(newsEntry.id).toNot.beNil();
+        expect(newsEntry.pubdate).to.equal(@"2014-04-03 10:28:04");
+        expect(newsEntry.content).toNot.beNil();
+        expect(newsEntry.title).toNot.beNil();
+        
+        NLEvent *event = _storage.events[0];
+        expect(event).to.beKindOf([NLEvent class]);
+        expect(event.id).to.equal(@1);
+        expect(event.categories).toNot.beNil();
+        expect(event.instagram).toNot.beNil();
+        expect(event.facebook).toNot.beNil();
+        expect(event.foursquare).toNot.beNil();
+        expect(event.images).toNot.beNil();
+        expect(event.startdate).to.equal(@"2013-08-13 08:00:00");
+        expect(event.title).toNot.beNil();
+        expect(event.summary).toNot.beNil();
+        expect(event.content).toNot.beNil();
+        expect(event.place).toNot.beNil();
+        
+        NLScreen *screen = _storage.screens[0];
+        expect(screen).to.beKindOf([NLScreen class]);
+        expect(screen.url).to.equal(@"creators");
+        expect(screen.fullname).toNot.beNil();
+        expect(screen.id).to.equal(@1);
+        expect(screen.image).toNot.beNil();
+        expect(screen.name).toNot.beNil();
+        
+        NLTeaser *teaser = _storage.teasers[0];
+        expect(teaser.pubdate).to.equal(@"2014-04-03 10:33:13");
+        expect(teaser.id).to.equal(@1);
+        expect(teaser.place).toNot.beNil();
+        expect(teaser.news).toNot.beNil();
+        expect(teaser.order).toNot.beNil();
+        expect(teaser.order).toNot.beNil();
+        expect(teaser.event).toNot.beNil();
+        
+        NLPlace *place = _storage.places[0];
+        expect(place.content).toNot.beNil();
+        expect(place.foursquare).toNot.beNil();
+        expect(place.instagram).toNot.beNil();
+        expect(place.images).toNot.beNil();
+        expect(place.id).to.equal(@1);
+        expect(place.geo).to.equal(@"54.4456,35.35");
+        expect(place.thumbnail).toNot.beNil();
+        expect(place.categories).toNot.beNil();
+    });
 });
 
 SpecEnd
