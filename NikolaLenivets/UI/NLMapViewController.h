ee6bb3f065546563c0790e4096fd6948f4114d6b
Semyon Novikov <novikov.s@prognoz.ru>
2014-06-23 09:22:55 +0600

    Обновил крашлитикс

---
+++ NikolaLenivets/UI/NLMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  NLMapViewController.h
+//  NikolaLenivets
+//
+//  Created by Semyon Novikov on 23.06.14.
+//  Copyright (c) 2014 Semyon Novikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NLMapViewController : UIViewController
+
+@end
---
+++ NikolaLenivets/UI/NLMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  NLMapViewController.m
+//  NikolaLenivets
+//
+//  Created by Semyon Novikov on 23.06.14.
+//  Copyright (c) 2014 Semyon Novikov. All rights reserved.
+//
+
+#import "NLMapViewController.h"
+
+@interface NLMapViewController ()
+
+@end
+
+@implementation NLMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
