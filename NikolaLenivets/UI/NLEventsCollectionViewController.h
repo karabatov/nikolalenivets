954a5fa8fb49ae11808554d0dd38b0016d5a4dc4
Yuri Karabatov <yuri@yurikarabatov.com>
2014-07-15 20:00:49 +0400

    События: небольшой док для NLEventsCollectionViewController.

--- NikolaLenivets/UI/NLEventsCollectionViewController.h
+++ NikolaLenivets/UI/NLEventsCollectionViewController.h
@@ -10,14 +10,34 @@
 #import "CHTCollectionViewWaterfallLayout.h"
 #import "NLEventGroup.h"
 
+/**
+ Used to display events in a Pinterest-like pattern using `CHTCollectionViewWaterfallLayout`.
+ */
 @interface NLEventsCollectionViewController : UIViewController <UICollectionViewDataSource, CHTCollectionViewDelegateWaterfallLayout>
 
+/**
+ Title on the (surprise) title bar.
+ */
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+/**
+ Unread items count.
+ */
 @property (weak, nonatomic) IBOutlet UILabel *itemsCountLabel;
+
+/**
+ Collection view to display events.
+ */
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
+/**
+ `Double cheeseburger` menu button pressed.
+ */
 - (IBAction)back:(id)sender;
 
+/**
+ Designated initializer to init the controller with an event group.
+ */
 - (instancetype)initWithGroup:(NLEventGroup *)group;
 
 @end
