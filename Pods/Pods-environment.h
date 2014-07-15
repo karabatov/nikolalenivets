cbdd8f638e6c30a2e46709d2a0b65f326a23f4dd
Yuri Karabatov <yuri@yurikarabatov.com>
2014-07-15 11:33:20 +0400

    Обновил Podfile: добавил CHTCollectionViewWaterfallLayout, обновился SDWebImage, поменялся метод setImageWithURL.

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -54,6 +54,12 @@
 #define COCOAPODS_VERSION_MINOR_AsyncImageView 5
 #define COCOAPODS_VERSION_PATCH_AsyncImageView 1
 
+// CHTCollectionViewWaterfallLayout
+#define COCOAPODS_POD_AVAILABLE_CHTCollectionViewWaterfallLayout
+#define COCOAPODS_VERSION_MAJOR_CHTCollectionViewWaterfallLayout 0
+#define COCOAPODS_VERSION_MINOR_CHTCollectionViewWaterfallLayout 7
+#define COCOAPODS_VERSION_PATCH_CHTCollectionViewWaterfallLayout 0
+
 // DCKeyValueObjectMapping
 #define COCOAPODS_POD_AVAILABLE_DCKeyValueObjectMapping
 #define COCOAPODS_VERSION_MAJOR_DCKeyValueObjectMapping 1
@@ -99,13 +105,13 @@
 // SDWebImage
 #define COCOAPODS_POD_AVAILABLE_SDWebImage
 #define COCOAPODS_VERSION_MAJOR_SDWebImage 3
-#define COCOAPODS_VERSION_MINOR_SDWebImage 6
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
 #define COCOAPODS_VERSION_PATCH_SDWebImage 0
 
 // SDWebImage/Core
 #define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
 #define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
-#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 6
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
 #define COCOAPODS_VERSION_PATCH_SDWebImage_Core 0
 
 // SKUTouchPresenter
