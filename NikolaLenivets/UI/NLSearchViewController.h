306871da2fa7f6993ed7cb32ffc0c1a4115073ba
Yuri Karabatov <yuri@yurikarabatov.com>
2014-09-12 18:37:56 +0400

    Поиск: шрифты, размеры.

--- NikolaLenivets/UI/NLSearchViewController.h
+++ NikolaLenivets/UI/NLSearchViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface NLSearchViewController : UIViewController
+@interface NLSearchViewController : UIViewController <UITextViewDelegate>
 
 @end
--- NikolaLenivets/UI/NLSearchViewController.m
+++ NikolaLenivets/UI/NLSearchViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "NLSearchViewController.h"
+#import "NSAttributedString+Kerning.h"
+#import "NLModel.h"
 
 @interface NLSearchViewController ()
 
@@ -29,6 +31,16 @@
  Side size for search image.
  */
 @property (strong, nonatomic) NSLayoutConstraint *searchImageSide;
+
+/**
+ Search text field.
+ */
+@property (strong, nonatomic) UITextView *searchField;
+
+/**
+ Placeholder label.
+ */
+@property (strong, nonatomic) UILabel *placeholderLabel;
 
 @end
 
@@ -59,13 +71,38 @@
     self.backButton.hidden = YES;
     self.backButton.alpha = 0.f;
 
+    self.searchField = [[UITextView alloc] init];
+    [self.searchField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.searchField.backgroundColor = [UIColor clearColor];
+    [self.searchField setReturnKeyType:UIReturnKeySearch];
+    [self.searchField setScrollEnabled:NO];
+    self.searchField.hidden = YES;
+    self.searchField.alpha = 0.f;
+    self.searchField.delegate = self;
+    self.searchField.typingAttributes = [self defaultSearchAttributes];
+
+    self.placeholderLabel = [[UILabel alloc] init];
+    [self.placeholderLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.placeholderLabel setTextAlignment:NSTextAlignmentCenter];
+    NSDictionary *attributes = @{ NSFontAttributeName: [UIFont fontWithName:NLMonospacedBoldFont size:37],
+                                  NSForegroundColorAttributeName: [UIColor colorWithRed:202.f/255.f green:202.f/255.f blue:202.f/255.f alpha:1.f],
+                                  NSKernAttributeName: [NSNumber numberWithFloat:4.3f] };
+    self.placeholderLabel.attributedText = [[NSAttributedString alloc] initWithString:@"ПОИСК" attributes:attributes];
+    self.placeholderLabel.alpha = 0.f;
+    self.placeholderLabel.hidden = YES;
+
+    [self.view addSubview:self.placeholderLabel];
     [self.view addSubview:self.searchImageView];
     [self.view addSubview:self.backButton];
+    [self.view addSubview:self.searchField];
 
-    NSDictionary *views = @{ @"sImg": self.searchImageView, @"back": self.backButton };
-    NSDictionary *metrics = @{ @"backSize": @44, @"backTop": @3.5 };
+    NSDictionary *views = @{ @"sImg": self.searchImageView, @"back": self.backButton, @"search": self.searchField, @"phl": self.placeholderLabel };
+    NSDictionary *metrics = @{ @"backSize": @44, @"backTop": @3.5, @"searchMargin": @17, @"searchV": [NSNumber numberWithFloat:-13.5f], @"phlV": [NSNumber numberWithFloat:-10.5f] };
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[back(backSize)]" options:kNilOptions metrics:metrics views:views]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-backTop-[back(backSize)]" options:kNilOptions metrics:metrics views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-searchMargin-[search]-searchMargin-|" options:kNilOptions metrics:metrics views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[back(backSize)]" options:kNilOptions metrics:metrics views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-backTop-[back(backSize)]-searchV-[search]-(>=0)-|" options:kNilOptions metrics:metrics views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[back]-phlV-[phl]" options:kNilOptions metrics:metrics views:views]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.backButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.f constant:0.f]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.searchImageView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.f constant:0.f]];
     CGFloat searchTop = [UIScreen mainScreen].bounds.size.height / 6.f - 17.f - 62.f;
@@ -74,6 +111,7 @@
     self.searchImageSide = [NSLayoutConstraint constraintWithItem:self.searchImageView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.f constant:62.f];
     [self.view addConstraint:self.searchImageSide];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.searchImageView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.searchImageView attribute:NSLayoutAttributeHeight multiplier:1.f constant:0.f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.placeholderLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.f constant:2.f]];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -90,11 +128,16 @@
         } completion:^(BOOL finished) {
             self.searchImageView.image = [self.searchImageView.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
             self.backButton.hidden = NO;
+            self.searchField.hidden = NO;
+            self.placeholderLabel.hidden = NO;
             [UIView animateWithDuration:0.15f animations:^{
                 self.backButton.alpha = 1.f;
+                self.searchField.alpha = 1.f;
+                self.placeholderLabel.alpha = 1.f;
                 self.searchImageView.alpha = 0.5f;
             } completion:^(BOOL finished) {
                 self.searchImageView.hidden = YES;
+                [self.searchField becomeFirstResponder];
             }];
         }];
     }
@@ -105,6 +148,8 @@
     self.searchImageView.hidden = NO;
     [UIView animateWithDuration:0.15f animations:^{
         self.backButton.alpha = 0.f;
+        self.searchField.alpha = 0.f;
+        self.placeholderLabel.alpha = 0.f;
         self.searchImageView.alpha = 1.0f;
     } completion:^(BOOL finished) {
         self.backButton.hidden = YES;
@@ -118,5 +163,39 @@
         }];
     }];
 }
+
+- (NSDictionary *)defaultSearchAttributes
+{
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.hyphenationFactor = 0.1f;
+    paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+    paragraphStyle.lineSpacing = 0.0f;
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    paragraphStyle.maximumLineHeight = 39.f;
+    NSDictionary *attributes = @{ NSFontAttributeName: [UIFont fontWithName:NLMonospacedBoldFont size:37],
+                                  NSForegroundColorAttributeName: [UIColor colorWithRed:37.f/255.f green:37.f/255.f blue:37.f/255.f alpha:1.f],
+                                  NSKernAttributeName: [NSNumber numberWithFloat:4.3f],
+                                  NSParagraphStyleAttributeName: paragraphStyle };
+    return attributes;
+}
+
+#pragma mark - UITextViewDelegate protocol
+
+- (void)textViewDidBeginEditing:(UITextView *)textView
+{
+    // textView.typingAttributes = [self defaultSearchAttributes];
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    textView.attributedText = [[NSAttributedString alloc] initWithString:[textView.text uppercaseString] attributes:[self defaultSearchAttributes]];
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView
+{
+    textView.typingAttributes = [self defaultSearchAttributes];
+}
+
+
 
 @end
